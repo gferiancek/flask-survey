13ef77f53c2a3fdf19b89b5b4c2014134333b1b7
Gavin Feriancek <gferiancek@gmail.com>
2023-12-28 00:11:18 -0500

    Added Survey Selection page and GET/POST for '/'

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
+from surveys import surveys
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'flasksurvey'
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config["SECRET_KEY"] = "flasksurvey"
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+current_survey = None
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    """Home page for the survey site.
+    GET - Renders form to select desired survey.
+    POST - Grabs desired survey and renders instructions page."""
+
+    if request.method == "GET":
+        return render_template("index.html", surveys=surveys.keys())
+    
+    if request.method == "POST":
+        survey_id = request.form.get("survey_id")
+        current_survey = surveys[survey_id]
+
+        return render_template(
+            "instructions.html",
+            title=current_survey.title,
+            instructions=current_survey.instructions,
+        )
